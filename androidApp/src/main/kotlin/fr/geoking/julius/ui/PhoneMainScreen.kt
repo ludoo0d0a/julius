9ca97a08387e8ac22adcb5555b03991d796077dd
LudoO <ludovic.valente@gmail.com>
2026-03-16 05:37:51 +0100

    Enhance PhoneMainScreen with state delegation for improved composability
    - Added `getValue` import to PhoneMainScreen to facilitate state delegation, enhancing the composability of the UI components.

--- androidApp/src/main/kotlin/fr/geoking/julius/ui/PhoneMainScreen.kt
+++ androidApp/src/main/kotlin/fr/geoking/julius/ui/PhoneMainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
